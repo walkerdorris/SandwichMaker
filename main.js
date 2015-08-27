cc7bf39ac7334e94094aa2eaf3aa2afa879226e3
Lauren Rouse <lauren.l.rouse@gmail.com>
2015-08-26 19:16:18 -0500

    main.js and meat.js work

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
 $(document).ready(function() {
 
-var details = [];
+var Sandwich = {};
 
+var button = document.getElementById("create_sandwich");
+var orderlist = document.getElementById("sandwich_here");
+var selections = {
+  "bread": [],
+  "cheese": [],
+  "condiments": [],
+  "meat": [],
+  "veggies": []
+};
 
-function food () {
-  if ($("input[name='bread')]:checked").val() === "white") {
-    details.push = "White";
-    console.log(details);
+var details = ["bread","cheese","condiments","meat","veggies"];
+
+function getValues () {
+  for (var i = 0; i < details.length; i++) {
+    var obj = $('input[name='+details[i]+']:checked');
+    // var j = $('input[name='+orderSections[i]+']:checked');
+    $(obj).each(function(index,type){
+      selections[details[i]].push($(this).val());
+    });
   }
+  console.log(selections);
+  Sandwich.meat(details.meat);
+  Sandwich.bread(details.bread);
+  Sandwich.condiments(details.condiments);
+  Sandwich.cheese(details.cheese);
+  Sandwich.veggies(details.veggies);
+
 }
 
-$("#create_sandwich").click(function() {
-  food();
-});
+button.addEventListener("click", getValues);
 
 });
--- meat.js
+++ meat.js
@@ -1,9 +1,18 @@
-var meat = (function() {
-  var meat_price {
+var Sandwich = (function(sandwich) {
+  var choices ={
     "turkey": "$1.00",
   	"ham": "$1.50",
   	"steak": "$2.00",
   	"salami": "$1.00"
+  };
+
+  sandwich.meat = function(input) {
+    for (var i = 0; i < input.length; i++) {
+      if (input[i] in choices) {
+        sandwich_here.innerHTML += input[i] + " " + choices[input[i]] + " ;";
+      }
+    }
   }
+  return sandwich;
 
 })(Sandwich);

f689fb0a7eee11de615d985be162656a7db1cf4f
Lauren Rouse <lauren.l.rouse@gmail.com>
2015-08-26 20:08:09 -0500

    clones

--- veggies.js
+++ veggies.js
@@ -1,10 +1,18 @@
-var veggie = (function() {
-  var veggie_price {
-  "lettuce": "$1.00",
-	"tomato": "$1.50",
-	"cucumber": "$1.00",
-	"sprouts": "$1.00"
-}
+var Sandwich = (function(sandwich) {
+  var choices ={
+    "lettuce": "$1.00",
+    "tomato": "$1.50",
+    "cucumber": "$1.00",
+    "sprouts": "$1.00"
+  };
 
+  sandwich.veggies = function(input) {
+    for (var i = 0; i < input.length; i++) {
+      if (input[i] in choices) {
+        sandwich_here.innerHTML += input[i] + " " + choices[input[i]] + " ;";
+      }
+    }
+  }
+  return sandwich;
 
 })(Sandwich);
